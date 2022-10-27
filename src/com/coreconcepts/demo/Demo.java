6f616a5946bfc9f6d745e248a0944c8fb6548254
svyashas <svyashas@inlen8520023564.corp.capgemini.com>
2022-10-27 20:17:04 +0530

    First Commit.

---
+++ src/com/coreconcepts/demo/Demo.java
@@ -0,0 +1,14 @@
+package com.coreconcepts.demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int x=10;
+		System.out.println(x);
+		for(int i=0;i<10;i++) {
+			x+=10;
+			System.out.println(x);
+		}
+	}
+
+}
